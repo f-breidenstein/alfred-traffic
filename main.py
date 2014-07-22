0f75f35340f7aec71f360ad2d39350cafe214d6d
Zoran Zaric <zz@zoranzaric.de>
2014-07-22 20:30:23 +0200

    Add database persistence of traffic

--- main.py
+++ main.py
@@ -3,16 +3,59 @@ import json
 import subprocess
 from jinja2 import Environment, FileSystemLoader
 import os
+import sqlite3
+
+CREATE_TABLE_SQL = """
+    CREATE TABLE IF NOT EXISTS
+        traffic
+        (node_name text,
+         traffic_sum int,
+         traffic_last int);"""
+
+GET_TRAFFIC_SQL = """
+    SELECT
+        traffic_sum,
+        traffic_last
+    FROM traffic
+    WHERE node_name = ?;"""
+
+UPDATE_TRAFFIC_SQL = """
+    UPDATE
+        traffic
+    SET
+        traffic_sum = ?,
+        traffic_last = ?;"""
 
 
 def get_key(item):
     return item['total']
 
 
+def create_table(conn):
+    c = conn.cursor()
+    c.execute(CREATE_TABLE_SQL)
+
+
+def get_traffic(conn, node_name):
+    c = conn.cursor()
+    c.execute(GET_TRAFFIC_SQL, (node_name,))
+    traffic_sum, traffic_last = c.fetchone()
+    return traffic_sum, traffic_last
+
+
+def update_traffic(conn, node_name, traffic_sum, traffic_last):
+    c = conn.cursor()
+    c.execute(UPDATE_TRAFFIC_SQL, (traffic_sum, traffic_last))
+    conn.commit()
+
+
 if __name__ == "__main__":
     path = os.path.dirname(os.path.realpath(__file__))
     env = Environment(loader=FileSystemLoader(path))
     template = env.get_template('layout.html')
+
+    conn = sqlite3.connect('data.sqlite')
+    create_table(conn)
 
     output = subprocess.check_output(["alfred-json","-r","158","-f","json","-z"])
     alfred = json.loads(output.decode("utf-8"))
@@ -42,10 +85,21 @@ if __name__ == "__main__":
         # Swap rx/tx beauce  it's meassured on the bat interface and not
         # on the Wifi interface. 
         # Rx on Bat0 == Tx on WiFi
-        node['rx'] = node_json['statistics']['traffic']['tx']['bytes'] / 1000000
-        node['tx'] = node_json['statistics']['traffic']['rx']['bytes'] / 1000000
-        node['total'] = node['rx'] + node['tx']
+        traffic_rx = node_json['statistics']['traffic']['tx']['bytes'] / 1000000
+        traffic_tx = node_json['statistics']['traffic']['rx']['bytes'] / 1000000
+        node['total'] = traffic_rx + traffic_tx
+
+        traffic_sum, traffic_last = get_traffic(conn)
+        if node['total'] >= traffic_last:
+            traffic_sum += node['total'] - traffic_last
+        else:
+            traffic_sum += node['total']
+        update_traffic(conn,
+                       traffic_sum=traffic_sum,
+                       traffic_last=node['total'])
+
         network.append(node)
 
     sorted = sorted(network, key=get_key, reverse=True)
     print (template.render(data=sorted))
+
